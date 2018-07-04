910c8fd3d57dde7ec7baa901f1feff35175a3228
fullyalive <wogud1202@gmail.com>
2018-07-04 16:48:45 +0900

    Handling Authentication with Redux and React Router

--- frontend/src/components/App/presenter.js
+++ frontend/src/components/App/presenter.js
@@ -1,15 +1,28 @@
-import React, { Component } from "react";
+import React from "react";
+import { Route, Switch } from "react-router-dom";
 import styles from "./styles.scss";
 import Footer from "components/Footer";
 
-class App extends Component {
-  render() {
-    return (
-      <div className={styles.App}>
-        <Footer />
-      </div>
-    );
-  }
-}
+const App = props => [
+    // Nav,
+    props.isLoggedIn ? <PrivateRoutes key={2} /> : <PublicRoutes key={2} />,
+    <Footer key={3} />
+];
+
+// 유저 로그인 시 노출
+const PrivateRoutes = props => (
+    <Switch>
+        <Route exact path="/" render={() => "login_feed"} />
+        <Route exact path="/explore" render={() => "login_explore"} />
+    </Switch>
+)
+
+// 비로그인 유저에게도 노출
+const PublicRoutes = props => (
+    <Switch>
+        <Route exact path="/" render={() => "feed"} />
+        <Route exact path="/explore" rende={() => "explore"} />
+    </Switch>
+)
 
 export default App;
