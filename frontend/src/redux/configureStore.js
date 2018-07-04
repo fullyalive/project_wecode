01a9f1c53f1ad3da8c87f94c9cfc353ec8d7d79a
fullyalive <wogud1202@gmail.com>
2018-07-04 16:24:55 +0900

    Making the App Container

--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -1,10 +1,10 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 import { routerReducer, routerMiddleware } from "react-router-redux";
 import createHistory from "history/createBrowserHistory";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { i18nState } from "redux-i18n";
-import thunk from "redux-thunk";
-import users from "redux/modules/users";
+import user from "redux/modules/user";
 
 const env = process.env.NODE_ENV;
 
@@ -18,7 +18,7 @@ if (env === "development") {
 }
 
 const reducer = combineReducers({
-  users,
+  user,
   routing: routerReducer,
   i18nState
 });
