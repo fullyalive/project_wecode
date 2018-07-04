11dbe5764a165ff2ca04a65b09a9c989bd5f5c5f
fullyalive <wogud1202@gmail.com>
2018-07-04 19:29:47 +0900

    Auth Component

--- frontend/src/components/Auth/presenter.js
+++ frontend/src/components/Auth/presenter.js
@@ -1,55 +1,42 @@
 import React from "react";
 import styles from "./styles.scss";
+import { LoginForm, SignupForm } from "components/AuthForms";
 
 const Auth = (props, context) => (
   <main className={styles.auth}>
     <div className={styles.row}>
-      <img
-        className={styles.logoImg}
-        src={require("images/logo.png")}
-        alt="wecode"
-      />
     </div>
     <div className={styles.row}>
-      <div className={styles.whiteBox}>
-        {(() => {
-          switch (props.action) {
-            case "login":
-              return (
-                <p>
-                  아직 회원이 아니신가요?{" "}
-                  <span
-                    onClick={props.changeAction}
-                    className={styles.changeLink}
-                  >
-                    회원가입
-                  </span>
-                </p>
-              );
-            case "signup":
-              return (
-                <p>
-                  회원이신가요?{" "}
-                  <span
-                    onClick={props.changeAction}
-                    className={styles.changeLink}
-                  >
-                    로그인
-                  </span>
-                </p>
-              );
-            default:
-              null;
-          }
-        })()}
+      <div className={`${styles.whiteBox} ${styles.formBox}`}>
+        <img src={require("images/logo.png")} alt="wecode" />
+        {props.action === "login" && <LoginForm />}
+        {props.action === "signup" && <SignupForm />}
       </div>
-      {/* <div className={styles.appBox}>
-        <span>Get the app</span>
-        <div className={styles.appstores}>
+      <div className={styles.whiteBox}>
+        {props.action === "login" && (
+          <p>
+            아직 회원이 아니신가요?{" "}
+            <span onClick={props.changeAction} className={styles.changeLink}>
+              회원가입
+            </span>
+          </p>
+        )}
+        {props.action === "signup" && (
+          <p>
+            회원이신가요?{" "}
+            <span onClick={props.changeAction} className={styles.changeLink}>
+              로그인
+            </span>
+          </p>
+        )}
+      </div>
+      <div className={styles.appBox}>
+        <span>wecode는 개발자의 자유를 꿈꿉니다</span>
+        {/* <div className={styles.appstores}>
           <img src={require("images/ios.png")} alt="" />
           <img src={require("images/ios.png")} alt="" />
-        </div>
-      </div> */}
+        </div> */}
+      </div>
     </div>
   </main>
 );
