5a2791b757948017cc03b641fc5f609fe52d7b6b
fullyalive <wogud1202@gmail.com>
2018-07-04 20:18:13 +0900

    Stateless Components and Context

--- frontend/src/components/Auth/presenter.js
+++ frontend/src/components/Auth/presenter.js
@@ -4,8 +4,7 @@ import { LoginForm, SignupForm } from "components/AuthForms";
 
 const Auth = (props, context) => (
   <main className={styles.auth}>
-    <div className={styles.row}>
-    </div>
+    <div className={styles.row} />
     <div className={styles.row}>
       <div className={`${styles.whiteBox} ${styles.formBox}`}>
         <img src={require("images/logo.png")} alt="wecode" />
@@ -15,23 +14,23 @@ const Auth = (props, context) => (
       <div className={styles.whiteBox}>
         {props.action === "login" && (
           <p className={styles.text}>
-            아직 회원이 아니신가요?{" "}
+            {context.t("아직 회원이 아니신가요?")}{" "}
             <span onClick={props.changeAction} className={styles.changeLink}>
-              회원가입
+              {context.t("회원가입")}
             </span>
           </p>
         )}
         {props.action === "signup" && (
           <p className={styles.text}>
-            회원이신가요?{" "}
+            {context.t("회원이신가요?")}{" "}
             <span onClick={props.changeAction} className={styles.changeLink}>
-              로그인
+              {context.t("로그인")}
             </span>
           </p>
         )}
       </div>
       <div className={styles.appBox}>
-        <span>wecode는 개발자의 자유를 꿈꿉니다</span>
+        <span>{context.t("wecode는 개발자의 자유를 꿈꿉니다")}</span>
         {/* <div className={styles.appstores}>
           <img src={require("images/ios.png")} alt="" />
           <img src={require("images/ios.png")} alt="" />
--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import styles from "./styles.scss";
 
 const Footer = (props, context) => (
@@ -6,22 +7,32 @@ const Footer = (props, context) => (
     <div className={styles.row}>
       <nav className={styles.nav}>
         <ul className={styles.list}>
-          <li className={styles.listItem}>서비스 소개</li>
-          <li className={styles.listItem}>이용약관</li>
-          <li className={styles.listItem}>개인정보 취급방침</li>
-          <li className={styles.listItem}>강의찾기</li>
-          <li className={styles.listItem}>스터디찾기</li>
-          <li className={styles.listItem}>커뮤니티</li>
+          <li className={styles.listItem}>{this.context.t("서비스 소개")}</li>
+          <li className={styles.listItem}>
+            {this.context.t("서비스 소개")}이용약관
+          </li>
+          <li className={styles.listItem}>
+            {this.context.t("개인정보 취급방침")}
+          </li>
+          <li className={styles.listItem}>{this.context.t("강의찾기")}</li>
+          <li className={styles.listItem}>{this.context.t("스터디찾기")}</li>
+          <li className={styles.listItem}>{this.context.t("커뮤니티")}</li>
         </ul>
       </nav>
     </div>
     <div className={styles.row}>
-      <p className={styles.about}>wecode | 대표 | 사업자등록번호 | 주소 <br/>
-      전화번호 | E-mail <br/>
+      <p className={styles.about}>
+        wecode | 대표 | 사업자등록번호 | 주소 <br />
+        전화번호 | E-mail <br />
       </p>
-      <span className={styles.copyright}>Copyright ©2018 wecode. All rights reserved</span>
+      <span className={styles.copyright}>
+        Copyright ©2018 wecode. All rights reserved
+      </span>
     </div>
   </footer>
 );
 
+Footer.contextTypes = {
+  t: PropTypes.func.isRequired
+}
 export default Footer;
