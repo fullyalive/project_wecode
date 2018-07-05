b41edfa41085f51b461d4c09779ad912b349d238
fullyalive <wogud1202@gmail.com>
2018-07-05 14:36:41 +0900

    Conrolling inputs and submit on LoginForm

--- frontend/src/components/LoginForm/presenter.js
+++ frontend/src/components/LoginForm/presenter.js
@@ -5,16 +5,22 @@ import formStyles from "shared/formStyles.scss";
 
 const LoginForm = (props, context) => (
     <div className={formStyles.formComponent}>
-        <form className={formStyles.form}>
+        <form className={formStyles.form} onSubmit={props.handleSubmit}>
             <input
                 type="text"
                 placeholder={context.t("아이디")}
                 className={formStyles.textInput}
+                value={props.usernameValue}
+                onChange={props.handleInputChange}
+                name="username"
             />
             <input
                 type="password"
                 placeholder={context.t("비밀번호")}
                 className={formStyles.textInput}
+                value={props.passwordValue}
+                onChnage={props.handleInputChange}
+                name="password"
             />
             <input
                 type="submit"
@@ -33,6 +39,12 @@ const LoginForm = (props, context) => (
     </div>
 );
 
+LoginForm.propTypes = {
+    usernameValue: PropTypes.string.isRequired,
+    passwordValue: PropTypes.string.isRequired,
+    handleInputChange: PropTypes.func.isRequired,
+    handleSubmit: PropTypes.func.isRequired
+}
 LoginForm.contextTypes = {
     t: PropTypes.func.isRequired
 };
