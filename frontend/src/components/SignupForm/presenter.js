8c77d5dcfa8d10f878b03b3a5c84ab0bcfd8a842
fullyalive <wogud1202@gmail.com>
2018-07-05 14:47:24 +0900

    Controlling inputs and submit on SignupForm

--- frontend/src/components/SignupForm/presenter.js
+++ frontend/src/components/SignupForm/presenter.js
@@ -13,40 +13,62 @@ const SignupForm = (props, context) => (
       {context.t("페이스북으로 로그인")}
     </button>
     <span className={formStyles.divider}>{context.t("또는")}</span>
-    <form className={formStyles.form}>
+    <form className={formStyles.form} onSubmit={props.handleSubmit}>
       <input
         type="email"
         placeholder={context.t("이메일")}
         className={formStyles.textInput}
+        value={props.emailValue}
+        onChange={props.handleInputChange}
+        name="email"
       />
       <input
         type="text"
         placeholder={context.t("이름")}
         className={formStyles.textInput}
+        value={props.fullnameValue}
+        onChange={props.handleInputChange}
+        name="fullname"
       />
       <input
         type="username"
         placeholder={context.t("아이디")}
         className={formStyles.textInput}
+        value={props.usernameValue}
+        onChange={props.handleInputChange}
+        name="username"
       />
       <input
         type="password"
         placeholder={context.t("비밀번호")}
         className={formStyles.textInput}
+        value={props.passwordValue}
+        onChange={props.handleInputChange}
+        name="password"
       />
       <input
         type="submit"
         value={context.t("회원가입")}
         className={formStyles.button}
+        onChange={props.handleSubmit}
       />
     </form>
     <p className={formStyles.terms}>
-      {context.t("회원가입을통해 ")}
+      {context.t("회원가입을 통해 ")}
       <span>{context.t("개인정보 취급방침과 이용약관")}</span>
       {context.t("에 동의하게 됩니다.")}
     </p>
   </div>
 );
+
+SignupForm.propTypes = {
+  emailValue: PropTypes.string.isRequired,
+  fullnameValue: PropTypes.string.isRequired,
+  usernameValue: PropTypes.string.isRequired,
+  passwordValue: PropTypes.string.isRequired,
+  handleInputChange: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired
+}
 
 SignupForm.contextTypes = {
     t: PropTypes.func.isRequired
