731fd7e7749d9f3efcfb309f454f1d64b1ea74b0
fullyalive <wogud1202@gmail.com>
2018-07-09 19:20:25 +0900

    RenderFeed Component and Persistence

--- frontend/src/components/Feed/presenter.js
+++ frontend/src/components/Feed/presenter.js
@@ -8,6 +8,8 @@ import Loading from "components/Loading";
 const Feed = props => {
   if (props.loading) {
     return <LoadingFeed />;
+  } else if (props.feed) {
+    return <RenderFeed {...props} />;
   }
 };
 
@@ -17,9 +19,13 @@ const LoadingFeed = props => (
   </div>
 );
 
+const RenderFeed = props => (
+  <div className={styles.feed}>{props.feed.map(post => post.comment)}</div>
+);
 
 Feed.propTypes = {
-  loading: PropTypes.bool.isRequired
+  loading: PropTypes.bool.isRequired,
+  feed: PropTypes.array
 };
 
 export default Feed;
--- frontend/src/redux/modules/photos.js
+++ frontend/src/redux/modules/photos.js
@@ -4,8 +4,16 @@ import { actionCreators as userActions } from "redux/modules/user";
 
 // actions
 
+const SET_FEED = "SET_FEED";
+
 // action creators
 
+function setFeed(feed) {
+  return {
+    type: SET_FEED,
+    feed
+  };
+}
 // api actions
 
 function getFeed() {
@@ -14,37 +22,45 @@ function getFeed() {
     //   user: { token }
     // } = getState();
     fetch("/lectures/")
-    // fetch("/lectures/", {
-    //   headers: {
-    //     Authorization: `JWT ${token}`
-    //   }
-    // })
+      // fetch("/lectures/", {
+      //   headers: {
+      //     Authorization: `JWT ${token}`
+      //   }
+      // })
       .then(response => {
         if (response.status === 401) {
           dispatch(userActions.logout());
         }
         return response.json();
       })
-      .then(json => console.log(json));
+      .then(json => dispatch(setFeed(json)));
   };
 }
 
 // initial state
 
-const initialState = {
-  feed: []
-};
+const initialState = {};
 
 // reducer
 
 function reducer(state = initialState, action) {
   switch (action.type) {
+    case SET_FEED:
+      return applySetFeed(state, action);
     default:
       return state;
   }
 }
 
 // reducer functions
+
+function applySetFeed(state, action) {
+  const { feed } = action;
+  return {
+    ...state,
+    feed
+  };
+}
 
 const actionCreators = {
   getFeed
