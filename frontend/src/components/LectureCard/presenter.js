128eb578c71ebf8da6fd2c880b94cd183c0c38fa
fullyalive <wogud1202@gmail.com>
2018-07-11 15:46:39 +0900

    Modifyung LectureComments component - Removing caption props

--- frontend/src/components/LectureCard/presenter.js
+++ frontend/src/components/LectureCard/presenter.js
@@ -19,16 +19,14 @@ const LectureCard = (props, context) => {
         </div>
       </header>
       {/* caption은 후에 강의 짧게 설명하는 것으로 바꿀것 */}
-      {/* 지금 사진 안뜬다 이것도 수정 */}
       {console.log(props)}
-      <img src={props.lectureImage} alt={props.caption} />
+      <img src={props.lectureImage} alt={props.short_description} />
       <div>
         {/* 숫자도 안뜨는 오류 있음 */}
-        {console.log(props)}
+        {/* {console.log(props)} */}
 
         <LectureActions number={props.like_count} />
         <LectureComments
-          caption={props.short_description}
           creator={props.creator.username}
           comments={props.lecture_comments}
         />
