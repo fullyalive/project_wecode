6c1d7df4ab0471c30358bd8acb8219499bf9c090
fullyalive <wogud1202@gmail.com>
2018-07-22 15:32:50 +0900

    Modifying path on index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { ConnectedRouter } from "react-router-redux";
-import store, { history, userContext } from "redux/configureStore";
+import store, { history } from "redux/configureStore";
+// import { userContext } from "redux/configureStore";
 import App from "components/App";
 import I18n from "redux-i18n";
 import { translations } from "translations";
-import { UserProvider } from "redux/userContext";
+import { UserProvider } from "redux/modules/userContext";
 
 ReactDOM.render(
   <Provider store={store}>
