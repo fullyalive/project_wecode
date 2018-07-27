f2562de5ab5a207817ab67b5c9a6d360385860de
fullyalive <wogud1202@gmail.com>
2018-07-27 13:21:05 +0900

    Add scale effect and margin to comment icon

--- frontend/src/components/CardDetails/CommentList/Comment/presenter.js
+++ frontend/src/components/CardDetails/CommentList/Comment/presenter.js
@@ -1,15 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Ionicon from "react-ionicons";
-import commentStyles from "shared/commentStyles.scss";
+import styles from "./styles.scss";
 
 const Comment = props => (
-  <li className={commentStyles.commentContainer}>
-    <span className={commentStyles.commentContent}>
-      <span className={commentStyles.username}>{props.creator}</span>
+  <li className={styles.commentContainer}>
+    <span className={styles.commentContent}>
+      <span className={styles.username}>{props.creator}</span>
       {props.isEdit ? (
         <textarea
-          className={commentStyles.editMessage}
+          className={styles.editMessage}
           value={props.currentComment}
           onChange={props.handleInputChange}
           onKeyPress={props.handleKeyPress}
@@ -17,16 +17,17 @@ const Comment = props => (
           {props.currentComment}
         </textarea>
       ) : (
-        <span className={commentStyles.message}>{props.comment}</span>
+        <span className={styles.message}>{props.comment}</span>
       )}
     </span>
-    <span className={commentStyles.commentFunction}>
+    <span className={styles.commentFunction}>
       {props.isEdit === false && props.creator === props.username ? (
         <Ionicon
           icon="ios-color-wand"
           fontSize="20px"
           color="#b4b4b4"
           onClick={props.onClick}
+          className={styles.icon}
         />
       ) : (
         ""
@@ -37,6 +38,7 @@ const Comment = props => (
           fontSize="24px"
           color="#b4b4b4"
           onClick={props.onDeleteClick}
+          className={styles.icon}
         />
       ) : (
         ""
