5458bd35e1d79d3f70be22d4263a5ddc7278622c
fullyalive <wogud1202@gmail.com>
2018-07-30 16:47:37 +0900

    Responsive Styling : Footer

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -28,6 +28,36 @@ const Footer = (props, context) => (
               </li>
             </ul>
           </div>
+          <div className={styles.mobileUpper}>
+            <ul className={styles.list}>
+              <span className={styles.listTitle}>SERVICE</span>
+              <li className={styles.listItem}>{context.t("서비스 소개")}</li>
+              <li className={styles.listItem}>{context.t("강의찾기")}</li>
+              <li className={styles.listItem}>{context.t("스터디찾기")}</li>
+              <li className={styles.listItem}>{context.t("커뮤니티")}</li>
+
+              <span className={styles.listTitle}>COMPANY</span>
+              <li className={styles.listItem}>{context.t("블로그")}</li>
+              <li className={styles.listItem}>{context.t("페이스북")}</li>
+            </ul>
+            <ul className={styles.list}>
+              <span className={styles.listTitle}>POLICIES</span>
+              <li className={styles.listItem}>{context.t("이용약관")}</li>
+              <li className={styles.listItem}>
+                {context.t("개인정보 취급방침")}
+              </li>
+              <span className={styles.listTitle}>SUPPORT</span>
+              <li className={styles.listItem}>
+                <img
+                  src={require("images/kakaoplus.png")}
+                  alt="wecode 플러스친구"
+                  className={styles.kakao}
+                />
+                {context.t("카카오 1:1 문의")}
+              </li>
+              <li className={styles.listItem}>{context.t("FAQ")}</li>
+            </ul>
+          </div>
           <div className={styles.row}>
             <p className={styles.about}>
               wecode | 대표: 이재형 | 사업자등록번호 | 주소 <br />
