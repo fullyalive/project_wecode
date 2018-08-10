d483b31fb1c53008d96e9a4ae5c95ddea3468158
fullyalive <wogud1202@gmail.com>
2018-08-11 01:27:07 +0900

    Adding specific color to postWriter

--- frontend/src/components/CardDetails/CommentList/Comment/presenter.js
+++ frontend/src/components/CardDetails/CommentList/Comment/presenter.js
@@ -5,105 +5,47 @@ import styles from "./styles.scss";
 
 const Comment = props => {
   const isParent = props.parent === 0 ? true : false;
-  
-  return (
-    <li className={styles.commentContainer}>
-      {!isParent && <span style={{ marginLeft: 30 }} />}
-      <div className={styles.commentContent}>
-        <div className={styles.commentHeader}>
-          <div className={styles.firstColumn}>
-            <span className={styles.username}>{props.creator}</span>
-            <span className={styles.createdTime}>
-              {props.created_time_mdhm}
-            </span>
+  const isWriter = props.creator === props.username? true: false;
+  const writerStyle = { color:"red"}
+  const readerStyle = { color:"black"}
+  return(<li className={styles.commentContainer}>
+        {!isParent && <span style={{ marginLeft: 30 }} />}
+        <div className={styles.commentContent}>
+          <div className={styles.commentHeader}>
+            <div className={styles.firstColumn}>
+              <span className={styles.username}>{props.creator}</span>
+              <span className={styles.createdTime}>
+                {props.created_time_mdhm}
+              </span>
+            </div>
+            <div className={styles.secondColumn}>
+              <span className={styles.commentFunction}>
+                {props.isLoggedIn && isParent && <Ionicon icon="md-return-right" fontSize="20px" color="#b4b4b4" onClick={props.onRecommentClick} className={styles.icon} />}
+                {props.isEdit === false && props.creator === props.username && <Ionicon icon="md-create" fontSize="20px" color="#b4b4b4" onClick={props.onClick} className={styles.icon} />}
+                {props.creator === props.username ? <Ionicon icon="md-close" fontSize="24px" color="#b4b4b4" onClick={isParent ? props.onDeleteClick : props.onRecommentDeleteClick} className={styles.icon} /> : ""}
+              </span>
+            </div>
           </div>
-          <div className={styles.secondColumn}>
-            <span className={styles.commentFunction}>
-              {props.isLoggedIn &&
-                isParent && (
-                  <Ionicon
-                    icon="ios-return-right"
-                    fontSize="20px"
-                    color="#b4b4b4"
-                    onClick={props.onRecommentClick}
-                    className={styles.icon}
-                  />
-                )}
-              {props.isEdit === false &&
-                props.creator === props.username && (
-                  <Ionicon
-                    icon="ios-color-wand"
-                    fontSize="20px"
-                    color="#b4b4b4"
-                    onClick={props.onClick}
-                    className={styles.icon}
-                  />
-                )}
-              {props.creator === props.username ? (
-                <Ionicon
-                  icon="ios-close"
-                  fontSize="24px"
-                  color="#b4b4b4"
-                  onClick={
-                    isParent
-                      ? props.onDeleteClick
-                      : props.onRecommentDeleteClick
-                  }
-                  className={styles.icon}
-                />
-              ) : (
-                ""
-              )}
-            </span>
+          <div className={styles.commentMessage}>
+            {props.isEdit ? <div>
+                <textarea  className={styles.editMessage} value={props.currentComment} onChange={props.handleInputChange} onKeyPress={props.handleKeyPress}>
+                  {props.currentComment}
+                </textarea>
+                <Ionicon icon="md-checkmark" fontSize="20px" color="#b4b4b4" onClick={props.onSubmitClick} className={styles.icon} />
+        </div> : <span style={isWriter ? writerStyle : readerStyle} className={styles.message}>
+                {props.comment}
+              </span>}
+          </div>
+          <div className={styles.commentMessage}>
+            {props.isReEdit && <div>
+                <textarea className={styles.editMessage} value={props.currentRecomment} onChange={props.handleRecommentInputChange} onKeyPress={props.handleRecommentKeyPress}>
+                  {props.currentComment}
+                </textarea>
+                <Ionicon icon="md-checkmark" fontSize="20px" color="#b4b4b4" onClick={props.onRecommentSubmitClick} className={styles.icon} />
+              </div>}
           </div>
         </div>
-        <div className={styles.commentMessage}>
-          {props.isEdit ? (
-            <div>
-              <textarea
-                className={styles.editMessage}
-                value={props.currentComment}
-                onChange={props.handleInputChange}
-                onKeyPress={props.handleKeyPress}
-              >
-                {props.currentComment}
-              </textarea>
-              <Ionicon
-                icon="md-checkmark"
-                fontSize="20px"
-                color="#b4b4b4"
-                onClick={props.onSubmitClick}
-                className={styles.icon}
-              />
-            </div>
-          ) : (
-            <span className={styles.message}>{props.comment}</span>
-          )}
-        </div>
-        <div className={styles.commentMessage}>
-          {props.isReEdit && (
-            <div>
-              <textarea
-                className={styles.editMessage}
-                value={props.currentRecomment}
-                onChange={props.handleRecommentInputChange}
-                onKeyPress={props.handleRecommentKeyPress}
-              >
-                {props.currentComment}
-              </textarea>
-              <Ionicon
-                icon="md-checkmark"
-                fontSize="20px"
-                color="#b4b4b4"
-                onClick={props.onRecommentSubmitClick}
-                className={styles.icon}
-              />
-            </div>
-          )}
-        </div>
-      </div>
-    </li>
-  );
+      </li>);
 };
 
 export default Comment;
