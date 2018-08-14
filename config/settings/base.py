45c8975783aa73fe803f263f5a260bed170ee4ca
fullyalive <wogud1202@gmail.com>
2018-08-14 12:59:30 +0900

    Fixing login error

--- config/settings/base.py
+++ config/settings/base.py
@@ -264,7 +264,6 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ),
